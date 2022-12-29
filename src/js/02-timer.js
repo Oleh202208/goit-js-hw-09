0f11f879dfb2f1d33fbf320791682b87b4893797
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2022-12-29 19:09:35 +0100

    )

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,16 +5,14 @@ const input = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('[data-start]');
 const timer = document.querySelector('.timer');
 
-const dataDays = document.querySelector('[data-days]');
-const dataHours = document.querySelector('[data-hours]');
-const dataMinutes = document.querySelector('[data-minutes]');
-const dataSeconds = document.querySelector('[data-seconds]');
+// const dataDays = document.querySelector('[data-days]');
+// const dataHours = document.querySelector('[data-hours]');
+// const dataMinutes = document.querySelector('[data-minutes]');
+// const dataSeconds = document.querySelector('[data-seconds]');
 
-// console.log(dataDays);
-startBtn.setAttribute('disabled', true);
+// startBtn.setAttribute('disabled', true);
 
 const data = new Date();
-
 
 const options = {
   enableTime: true,
