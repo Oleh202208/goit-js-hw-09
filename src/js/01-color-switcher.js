3081fd64cb6c8e2f9b4bad9fcf5fe07da001cf3c
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2022-12-29 18:45:57 +0100

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const bodyEl = document.querySelector('body')
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+console.log(bodyEl);
+let chenchColor = null;
+stopBtn.disabled = true;
+
+startBtn.addEventListener('click', () => {
+chenchColor = setInterval(()=>{
+bodyEl.style.backgroundColor = getRandomHexColor()
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+}, 1000, );
+}) 
+
+stopBtn.addEventListener('click', ()=>{
+  clearInterval(chenchColor);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+})
 
