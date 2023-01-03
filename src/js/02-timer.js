14b23e8942e06b78b209431a38ce8e8d7828e907
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2023-01-03 13:29:54 +0100

    gf

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,96 +2,96 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 
-// const input = document.querySelector('#datetime-picker');
-// const startBtn = document.querySelector('[data-start]');
-// const timer = document.querySelector('.timer');
+const input = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const timer = document.querySelector('.timer');
 
-// const dataDays = document.querySelector('[data-days]');
-// const dataHours = document.querySelector('[data-hours]');
-// const dataMinutes = document.querySelector('[data-minutes]');
-// const dataSeconds = document.querySelector('[data-seconds]');
+const dataDays = document.querySelector('[data-days]');
+const dataHours = document.querySelector('[data-hours]');
+const dataMinutes = document.querySelector('[data-minutes]');
+const dataSeconds = document.querySelector('[data-seconds]');
 
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   onClose(selectedDates) {
-//     console.log(selectedDates[0]);
-//   },
-// };
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
 
-// const flpCalendar = flatpickr(input, options,);
+const flpCalendar = flatpickr(input, options,);
 
-// let pickedTime;
-// let currentTime;
-// let intervalId = null;
+let pickedTime;
+let currentTime;
+let intervalId = null;
 
-// disableBtn(startBtn);
-// flpCalendar.config.onChange.push(dataChange);
-// startBtn.addEventListener('click', onStartBtnClick);
+disableBtn(startBtn);
+flpCalendar.config.onChange.push(dataChange);
+startBtn.addEventListener('click', onStartBtnClick);
 
-// function dataChange(){
-//   pickedTime = flpCalendar.selectedDates[0];
-// const differenceData = pickedTime - new Date();
+function dataChange(){
+  pickedTime = flpCalendar.selectedDates[0];
+const differenceData = pickedTime - new Date();
 
-// if (differenceData <= 0) {
-//   disableBtn(startBtn);
-//   flpCalendar.close();
-//   Notiflix.Notify.failure('Please choose a date in the future');
-// } else {
-//   enableBtn(startBtn);
-// }
-// }
+if (differenceData <= 0) {
+  disableBtn(startBtn);
+  flpCalendar.close();
+  Notiflix.Notify.failure('Please choose a date in the future');
+} else {
+  enableBtn(startBtn);
+}
+}
 
 
-// function onStartBtnClick() {
-//   intervalId = setInterval(() => {
-//   currentTime = new Date();
-//     let differenceTime = pickedTime - currentTime;
+function onStartBtnClick() {
+  intervalId = setInterval(() => {
+  currentTime = new Date();
+    let differenceTime = pickedTime - currentTime;
 
-//     if (differenceTime < 0) {
-//       clearInterval(intervalId);
-//       return;
-//     }
-//     changeTime(convertMs(differenceTime));
-//   }, 1000);
-// }
+    if (differenceTime < 0) {
+      clearInterval(intervalId);
+      return;
+    }
+    changeTime(convertMs(differenceTime));
+  }, 1000);
+}
 
-// function changeTime({ days, hours, minutes, seconds }) {
-//   dataDays.textContent = addLeadingZero(days);
-//   dataHours.textContent = addLeadingZero(hours);
-//   dataMinutes.textContent = addLeadingZero(minutes);
-//   dataSeconds.textContent = addLeadingZero(seconds);
-// }
+function changeTime({ days, hours, minutes, seconds }) {
+  dataDays.textContent = addLeadingZero(days);
+  dataHours.textContent = addLeadingZero(hours);
+  dataMinutes.textContent = addLeadingZero(minutes);
+  dataSeconds.textContent = addLeadingZero(seconds);
+}
 
-// function disableBtn(btn) {
-//   btn.setAttribute('disabled', true);
-// }
+function disableBtn(btn) {
+  btn.setAttribute('disabled', true);
+}
 
-// function enableBtn(btn) {
-//   btn.removeAttribute('disabled');
-// }
+function enableBtn(btn) {
+  btn.removeAttribute('disabled');
+}
 
-// function convertMs(ms) {
-//   // Number of milliseconds per unit of time
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-//   // Remaining days
-//   const days = Math.floor(ms / day);
-//   // Remaining hours
-//   const hours = Math.floor((ms % day) / hour);
-//   // Remaining minutes
-//   const minutes = Math.floor(((ms % day) % hour) / minute);
-//   // Remaining seconds
-//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-//   return { days, hours, minutes, seconds };
-// }
-// function addLeadingZero(value) {
-//   return value.toString().padStart(2, 0);
-// }
+  return { days, hours, minutes, seconds };
+}
+function addLeadingZero(value) {
+  return value.toString().padStart(2, 0);
+}
 
